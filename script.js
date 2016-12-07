5f1eaf2b73b40f41291fcd0c2824485137af1358
Johnson LI <johnsonli@uagateway.org>
2016-12-07 21:10:45 +0000

    This stuff with pixels and JAJVJAJVPAGHINDSGVPNVSPN

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Write your function calls below this line
+setColor(2,5,"red")
+setColor(2,6,"red")
+setColor(3,4,"red")
+setColor(4,5,"red")
+setColor(5,6,"red")
+setColor(6,5,"red")
+setColor(6,4,"red")
+setColor(0,0,"red")
+setColor(0,9,"red")
+setColor(9,0,"red")
+setColor(9,9,"red")
+
